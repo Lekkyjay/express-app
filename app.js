dc3a248a3392b494a13cfd17d109d35c6f636fe8
Lekky Jay <lekkyjay@yahoo.com>
2018-06-11 23:06:55 +0200

    Basic route added

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('path');
 
 var app = express();
 
+app.get('/', (req, res)=>{
+  res.send('Hello World!');
+});
+
 app.listen(3000, ()=>{
   console.log('Server started at port 3000...');
 });
