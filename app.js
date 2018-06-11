5c0dc008cef2e8d598dbca8060e916ced496e43c
Lekky Jay <lekkyjay@yahoo.com>
2018-06-11 23:03:30 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+var app = express();
+
+app.listen(3000, ()=>{
+  console.log('Server started at port 3000...');
+});
